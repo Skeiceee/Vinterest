364040762a09580a6a958cbf5b9863a1bcf67c99
Victor Nuñez <skeiceee@gmail.com>
2019-02-14 17:17:07 -0300

    ignore node_modules

--- src/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/vinterest_tutorial', {
-  useNewUrlParser: true
-})
-.then(db => console.log(`DB is connected`))
-.catch(err => console.error(err));
